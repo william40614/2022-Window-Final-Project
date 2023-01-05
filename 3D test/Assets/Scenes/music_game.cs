8be3efb94115bcf96d947de8d55b17ffe23cfc43
wen2wen2 <wendy2lai@gmail.com>
2023-01-06 00:24:55 +0800

    add bgm

---
+++ 3D test/Assets/Scenes/music_game.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class music_game : MonoBehaviour
+{
+    
+    public AudioSource audioSource;
+    public Button button;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //add a listener for the button's onclick event
+        button.onClick.AddListener(ToggleMusic);
+        if(audioSource.isPlaying) Debug.Log("playing music");
+    }
+
+    // Update is called once per frame
+    public void ToggleMusic()
+    {
+        if (audioSource.isPlaying)
+        {
+          
+            audioSource.Stop();
+        }
+        else
+        {
+            audioSource.Play();
+        }
+       //audioSource.isPlaying = !audioSource.isPlaying;
+    }
+}
---
+++ 3D test/Assets/Scenes/music_menu.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class music_menu : MonoBehaviour
+{
+
+
+    public AudioSource audioSource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
